c3cec9c0862fd329047a33c147ec408c76c8a3fb
ckk <1499381220@qq.com>
2022-04-05 12:08:16 +0800

    引入插件

---
+++ src/store/index.ts
@@ -0,0 +1,26 @@
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+const state = {
+    count: 100
+}
+// 
+const mutations = {
+ 
+}
+// 异步
+const actions = {
+   
+}
+// 获取
+const getters = {
+   
+}
+
+export default new Vuex.Store({
+    state,
+    mutations,
+    actions,
+    getters
+})
